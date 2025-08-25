beabb0109972de02d68da79fd7c85c3b3e391a1d
nmiller15 <nmiller@certifiedangusbeef.com>
2025-08-25 15:20:45 -0400

    temporarily disable fzf tab complete

--- windows/powershell/fzf.ps1
+++ windows/powershell/fzf.ps1
@@ -1,3 +1,4 @@
 Import-Module PSFzf
+
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+# Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
