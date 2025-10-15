e24f8f4018b765d3ad6ea5f5124ff30a7f78fe15
nmiller15 <nmiller@certifiedangusbeef.com>
2025-10-15 11:12:51 -0400

    add more tools to windows path

--- windows/powershell/path.ps1
+++ windows/powershell/path.ps1
@@ -1,3 +1,5 @@
 $env:PATH = "$env:PATH;$env:DOTFILES\shared\bin"
 $env:PATH = "$env:PATH;C:\Users\NMiller\AppData\Local\Microsoft\WinGet\Links"
 $env:PATH = "$env:PATH;C:\Program Files\ripgrep"
+$env:PATH = "$env:PATH;C:\Tools"
+$env:PATH = "$env:PATH;C:\Users\NMiller\AppData\Local\Programs\Python\Python313"
